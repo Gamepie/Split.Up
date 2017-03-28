9fe991f41c238668cdd52c004847e60d0b41636a
Alexandre Le Mieux <alexandre.lemieux@gamepie.fr>
2017-03-28 15:49:11 +0200

    Fix Home button

---
+++ Assets/Scripts/UnloadLevel.cs
@@ -0,0 +1,32 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using UnityEngine;
+using VoxelBusters.NativePlugins;
+using UnityEngine.SceneManagement;
+
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+		[ActionCategory("Scenemanager")]
+	[Tooltip("Unload")]
+	public class UnloadLevel : FsmStateAction
+
+	
+	{
+		
+
+
+		public override void OnEnter ()
+			{
+
+			SceneManager.UnloadSceneAsync (SceneManager.GetActiveScene().name);
+			Debug.Log ("unloaded");
+			Finish();
+
+			}
+				
+
+		}
+		
+}
